da6349e91dd438050719b58eacf65d228213292d
root <ohsungwoo@unist.ac.kr>
2025-10-23 14:50:18 +0000

    4090

--- AutomatedInstall.sh
+++ AutomatedInstall.sh
@@ -1,8 +1,8 @@
 conda create -n apc -y
 conda activate apc
-curl -fsSL https://ollama.com/install.sh | sh
 
+curl -fsSL https://ollama.com/install.sh | sh
 # ollama serve
-# ollama pull gpt-oss:20b
+ollama pull gpt-oss:20b
 
 pip install ollama openpyxl
